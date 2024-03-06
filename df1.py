da4241217fd926220f3859f982cf66647571a425
pkseeg <m.parker.seegmiller@gmail.com>
2024-03-06 09:59:44 -0500

    updated class

--- df1.py
+++ df1.py
@@ -1,40 +1,34 @@
 from sklearn.metrics import precision_recall_fscore_support
 import numpy as np
-import random
 
 from sentence_transformers import SentenceTransformer
 from tte_depth import StatDepth
 
 
-def _depth_scores_y(x, y, model_card):
-    model = SentenceTransformer(model_card)
+class DF1:
+    def __init__(self, source_texts, target_texts, model_card):
+        # FIXME add documentation -- I need to explain that the source_texts should be sampled, and the target_texts too
+        self.source_median, self.target_depths = self._depth_scores(source_texts, target_texts, model_card)
 
-    # encode all texts using the sentence transformer model
-    F = model.encode(x)
-    G = model.encode(y)
+    def _depth_scores_y(self, source_texts, target_texts, model_card):
+        # Load sentence transformer model
+        model = SentenceTransformer(model_card)
 
-    # depth_rank_test returns depth scores for each corpus, along with a Q estimate, W test statistic from the Wilcoxon Rank Sum Test, and an associated p-value
-    d = StatDepth()
-    _, depth_scores_G, _, _, _ = d.depth_rank_test(F, G)
+        # encode source and target texts using the sentence transformer model
+        encoded_source = model.encode(source_texts)
+        encoded_target = model.encode(target_texts)
 
-    # return 
-    print(f"Depth: {depth_scores_G}")
-    return depth_scores_G
+        # depth_rank_test returns depth scores for each corpus, along with a Q estimate, W test statistic from the Wilcoxon Rank Sum Test, and an associated p-value
+        d = StatDepth()
+        source_depths, target_depths, _, _, _ = d.depth_rank_test(encoded_source, encoded_target)
 
-def _softmax(x):
-    """Compute softmax values for each sets of scores in x."""
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum()
-
-
-def _create_sample_weights(x, y, train_n, model_card):
-    train_x = random.choices(x, k=train_n)
-    depth_y = _depth_scores_y(train_x, y, model_card=model_card)
-    sample_weights = _softmax(depth_y * -1)
-    print(f"Softmaxed: {sample_weights}")
-    return sample_weights
-
-def df1(y_true, y_pred, x, y, average="micro", train_n = 500, model_card = 'all-MiniLM-L6-v2'):
-    sample_weights = _create_sample_weights(x,y, train_n, model_card)
-    _, _, f_score, _ = precision_recall_fscore_support(y_true, y_pred, sample_weight=sample_weights, average=average)
-    return f_score
+        # calculate the median depth of the source_texts
+        source_median = np.median(source_depths)
+        return source_median, target_depths
+    
+    def precision_recall_fscore_support(self, y_true, y_pred, **kwargs):
+        # calculate the distances between the depth of the source median and the depth of each target text
+        dists = [self.source_median - x for x in self.target_depths]
+        # set the sample weights (for ease of access later)
+        self.sample_weights = [d/sum(dists) for d in dists]
+        return precision_recall_fscore_support(y_true, y_pred, sample_weight=self.sample_weights, **kwargs)
